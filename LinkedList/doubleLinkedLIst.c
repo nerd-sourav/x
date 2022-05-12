d266df392d72cd87ddefd84916e04fd474e5b398
nerd-sourav <souravprasad1869@gmail.com>
2022-05-13 01:34:13 +0530

    linked list doubly

---
+++ LinkedList/doubleLinkedLIst.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+struct Node *head = NULL;
+void append();
+void display();
+void main()
+{
+    // create a double linked list
+    for (int i = 0; i < 5; i++)
+    {
+        append();
+    }
+    display();
+}
+void append()
+{
+    struct Node *temp;
+    temp = (struct Node *)malloc(sizeof(struct Node));
+    printf("Enter Data: ");
+    scanf("%d", &temp->data);
+
+    if (head == NULL) // no elements are present
+    {
+        head = temp;
+    }
+    else
+    {
+        struct Node *p;
+        p = head;
+        while (p->right != NULL)
+        {
+            p = p->right;
+        }
+        temp->left = p;
+        p->right = temp;
+    }
+}
+
+void display()
+{
+    struct Node *temp;
+    temp = head;
+    while (temp != NULL)
+    {
+        printf("%d ", &temp->data);
+        temp = temp->right;
+    }
+}
