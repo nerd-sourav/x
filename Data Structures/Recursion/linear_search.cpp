b9cfc153db52c4bbd3ffbc80e9ea1343155fa2cb
nerd-sourav <souravprasad1869@gmail.com>
2022-05-26 21:29:51 +0530

    ls using recursion

---
+++ Data Structures/Recursion/linear_search.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+// int linearSearch(int arr[], int index, int n, int k)
+// {
+
+//     if (index == n)
+//         return -1;
+//     if (n == 0)
+//         return -1;
+//     if (n == 1)
+//         return 0;
+
+//     if (arr[index] == k)
+//         return index;
+
+//     return linearSearch(arr, index + 1, n, k);
+// }
+int linearSearch(int arr[], int n, int k)
+{
+
+    if (n == 0)
+        return 0;
+    if (arr[0] == k)
+        return true;
+    else
+    {
+        return linearSearch(arr + 1, n - 1, k);
+    }
+}
+
+int main()
+{
+    int n, k;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter Key" << endl;
+    cin >> k;
+    int ans = linearSearch(arr, n, k);
+    cout << ans;
+    return 0;
+}
