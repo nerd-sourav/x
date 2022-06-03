6a9208e11c8948ab6d844a913d4a538fd496296c
nerd-sourav <souravprasad1869@gmail.com>
2022-06-03 20:11:05 +0530

    ddfdf

--- Data Structures/Recursion/2powern.cpp
+++ Data Structures/Recursion/2powern.cpp
@@ -6,6 +6,10 @@ using namespace std;
 // changes made
 //  changes again made
 //  changes again made
+//hanges again made
+// changes made
+//  changes again made
+//  changes again made
 int power(int base, int pow)
 {
     if (pow == 0)
