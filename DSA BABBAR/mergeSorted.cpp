be2679995334974b5f3abd605580eae81eb6cade
nerd-sourav <souravprasad1869@gmail.com>
2022-06-12 20:32:06 +0530

    changes mades

--- DSA BABBAR/mergeSorted.cpp
+++ DSA BABBAR/mergeSorted.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 #include <cmath>
 using namespace std;
-//changes made
+// changes made
+// changes made
 void merge(int arr1[], int n, int arr2[], int m, int arr3[])
 {
     int i = 0, j = 0, k = 0;
