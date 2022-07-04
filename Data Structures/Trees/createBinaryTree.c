21c2b7e99d3d9b6a7a8452613775ce1ebdc5b40a
nerd-sourav <souravprasad1869@gmail.com>
2022-07-04 22:15:55 +0530

    changes made

---
+++ Data Structures/Trees/createBinaryTree.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+//changes made
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *create()
+{
+    int x;
+    struct node *newNode;
+    newNode = (struct node *)malloc(sizeof(struct node *));
+    printf("Entr -1 for no node");
+    scanf("%d", &x);
+    if (x == -1)
+    {
+        return 0;
+    }
+    newNode->data = x;
+    printf("Enter left child of %d", x);
+    newNode->left = create();
+    printf("Enter right child");
+    newNode->right = create();
+    return newNode;
+}
+
+int main()
+{
+    struct node *root = NULL;
+    root = create();
+}
