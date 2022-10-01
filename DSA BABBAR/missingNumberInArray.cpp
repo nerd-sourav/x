0ef006d9111bdf26f8f6a4b796cb8b4526bd8391
nerd-sourav <souravprasad1869@gmail.com>
2022-10-01 22:16:33 +0530

    maintainer file added

--- DSA BABBAR/missingNumberInArray.cpp
+++ DSA BABBAR/missingNumberInArray.cpp
@@ -4,16 +4,6 @@ using namespace std;
 // using XOR Oper
 int main()
 {
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    return 0;
+    int a = 3;
+    int b = 10;
 }
