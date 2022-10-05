cb8f45406f35bdaf2d5427842c698fd971dd6926
nerd-sourav <souravprasad1869@gmail.com>
2022-10-05 22:42:47 +0530

    geek for geeks added

---
+++ GFG POTD/5_oct_geekandstring.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    vector<int> prefixCount(int N, int Q, string li[], string query[])
+    {
+        // code here
+        unordered_map<string, int> maps;
+
+        for (int i = 0; i < N; i++)
+        {
+            string s = "";
+            for (int j = 0; j < li[i].size(); j++)
+            {
+                s += li[i][j];
+                maps[s] += 1;
+            }
+        }
+        vector<int> answer(Q, 0);
+        for (int i = 0; i < Q; i++)
+        {
+            answer[i] = maps[query[i]];
+        }
+
+        return answer;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int Q, N, i = 0, x;
+        cin >> N;
+        string s;
+        string li[N];
+        for (int i = 0; i < N; i++)
+            cin >> li[i];
+        cin >> Q;
+        x = Q;
+        string query[Q];
+        while (Q--)
+        {
+            cin >> s;
+            query[i++] = s;
+        }
+
+        Solution ob;
+        vector<int> ans = ob.prefixCount(N, x, li, query);
+        for (auto i : ans)
+            cout << i << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
