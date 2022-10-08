ba49a98562e7408bf9f334ea52d1669a67d32782
nerd-sourav <souravprasad1869@gmail.com>
2022-10-08 21:07:23 +0530

    c

---
+++ GFG POTD/8_oct_substringSlidingWindow.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    int countOfSubstrings(string str, int k)
+    {
+        // code here
+        int count = 0;
+        int s = 0;
+        unordered_map<char, int> mp;
+        for (int i = 0; i < str.size(); i++)
+        {
+            mp[str[i]]++;
+            if (i + 1 >= k)
+            {
+                if (mp.size() == k - 1)
+                {
+                    count++;
+                }
+                mp[str[s]]--;
+                if (mp[str[s]] == 0)
+                {
+                    mp.erase(str[s]);
+                }
+                s++;
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S;
+        int K;
+        cin >> S;
+        cin >> K;
+
+        Solution ob;
+        cout << ob.countOfSubstrings(S, K) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
