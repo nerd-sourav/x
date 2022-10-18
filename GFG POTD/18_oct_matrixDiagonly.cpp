0df3f2ce207f59277d07e81627193a18f56e7ec2
nerd-sourav <souravprasad1869@gmail.com>
2022-10-18 22:02:36 +0530

    gfg

---
+++ GFG POTD/18_oct_matrixDiagonly.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+
+public:
+    vector<int> downwardDigonal(int n, vector<vector<int>> a)
+    {
+        // Your code goes here
+        vector<int> ans;
+        // for upper triangle
+        for (int i = 0; i < n; i++)
+        {
+            int row = 0;
+            int col = i;
+            while (col >= 0)
+            {
+                ans.push_back(a[row][col]);
+                row++;
+                col--;
+            }
+        }
+        // lower triangle
+        for (int i = 1; i < n; i++)
+        {
+            int row = i;
+            int col = n - 1;
+            while (row < n)
+            {
+                ans.push_back(a[row][col]);
+                row++;
+                col--;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<vector<int>> A(n, vector<int>(n));
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                cin >> A[i][j];
+
+        Solution obj;
+        vector<int> ans = obj.downwardDigonal(n, A);
+
+        for (auto i : ans)
+            cout << i << " ";
+
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
