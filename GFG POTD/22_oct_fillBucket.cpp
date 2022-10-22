50b88281c4344f0bf50e2197126ab256b5a47aab
nerd-sourav <souravprasad1869@gmail.com>
2022-10-22 22:26:03 +0530

    gfg

---
+++ GFG POTD/22_oct_fillBucket.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int fillingBucket(int n)
+    {
+        // code here
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        int dp[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++)
+        {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 100000000;
+        }
+        return dp[n];
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+
+        cin >> N;
+
+        Solution ob;
+        cout << ob.fillingBucket(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
