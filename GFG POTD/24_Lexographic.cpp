695be7d2d46f0237024d8332e9959dd9b8adbce2
nerd-sourav <souravprasad1869@gmail.com>
2022-10-24 22:20:41 +0530

    gfg

---
+++ GFG POTD/24_Lexographic.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+struct alphanumeric
+{
+    string name;
+    int count;
+};
+class Solution
+{
+public:
+    vector<alphanumeric> sortedStrings(int n, vector<string> st)
+    {
+        // code here
+        vector<alphanumeric> ans;
+        map<string, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            mp[st[i]]++;
+        }
+        for (auto i : mp)
+            ans.push_back({i.first, i.second});
+        // cout << i.first << " " << i.second
+        //      << endl;
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        cin.ignore();
+        vector<string> v;
+        for (int i = 0; i < N; i++)
+        {
+            string s;
+            getline(cin, s);
+            v.push_back(s);
+        }
+        Solution ob;
+        vector<alphanumeric> ans = ob.sortedStrings(N, v);
+        for (auto u : ans)
+            cout << u.name << " " << u.count << "\n";
+    }
+}
+// } Driver Code Ends
