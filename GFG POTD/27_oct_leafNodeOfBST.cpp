b873a43b1cb0ec54252a0851754907ebd58c0266
nerd-sourav <souravprasad1869@gmail.com>
2022-10-27 21:09:23 +0530

    gfg

---
+++ GFG POTD/27_oct_leafNodeOfBST.cpp
@@ -0,0 +1,67 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    vector<int> leafNodes(int arr[], int n)
+    {
+        // code here
+        vector<int> ans;
+        stack<int> stk;
+
+        for (int i = 0, j = 1; j < n; i++, j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                stk.push(arr[i]);
+            }
+            else
+            {
+                bool found = false;
+                while (!stk.empty())
+                {
+                    if (arr[j] > stk.top())
+                    {
+                        stk.pop();
+                        found = true;
+                    }
+                    else
+                        break;
+                }
+                if (found)
+                {
+                    ans.push_back(arr[i]);
+                }
+            }
+        }
+        ans.push_back(arr[n - 1]);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++)
+            cin >> arr[i];
+        Solution ob;
+        vector<int> ans = ob.leafNodes(arr, N);
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
