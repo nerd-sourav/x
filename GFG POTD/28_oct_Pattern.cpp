b72314ab2dee37cdb6df86082f6cdb2d2ff9dbf4
nerd-sourav <souravprasad1869@gmail.com>
2022-10-28 13:01:16 +0530

    gfg

--- GFG POTD/28_oct_Pattern.cpp
+++ GFG POTD/28_oct_Pattern.cpp
@@ -1,39 +1,39 @@
 
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-//User function Template for C++
+class Solution
+{
+public:
+    int NthTerm(int n)
+    {
+        // Code  here
+        long ans = 1;
+        long mod = 1000000007;
+        for (int i = 1; i <= n; i++)
+        {
+            ans = ((ans * i) + 1) % mod;
+            //  cout<<ans  <<endl;
+        }
 
-class Solution {
-	public:
-		int NthTerm(int n){
-		    // Code  here
-		    long ans = 1;
-		    long mod = 1000000007;
-		    for(int i = 1; i<=n;i++){
-		        ans = ((ans*i)+1)%mod;
-		      //  cout<<ans  <<endl;
-		    }
-		    
-		    
-		    return ans;
-		}
-
+        return ans;
+    }
 };
 
 //{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n;
-		cin >> n;
-		Solution obj;
-		int ans = obj.NthTerm(n);
-		cout << ans << "\n";
-	}
-	return 0;
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        Solution obj;
+        int ans = obj.NthTerm(n);
+        cout << ans << "\n";
+    }
+    return 0;
 }
 // } Driver Code Ends
