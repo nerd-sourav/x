9e87de5743c35feca0f5a72636869d1c8167db7a
nerd-sourav <souravprasad1869@gmail.com>
2022-10-30 21:45:34 +0530

    gfg

---
+++ GFG POTD/30_oct_noOfChicks.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    long long int NoOfChicks(int n)
+    {
+        long newChick[n + 1];
+        long chick = 1;
+        newChick[0] = 1;
+
+        for (int day = 1; day < n; day++)
+        {
+
+            if (day >= 6)
+            {
+                chick -= newChick[day - 6];
+            }
+            newChick[day] = chick * 2;
+            chick += newChick[day];
+        }
+        return chick;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int N;
+        cin >> N;
+        Solution obj;
+        long long int ans = obj.NoOfChicks(N);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
