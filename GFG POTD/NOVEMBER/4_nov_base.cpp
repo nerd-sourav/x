faee00a4fb0d5e25ffc2e4520bd0f4669283650a
nerd-sourav <souravprasad1869@gmail.com>
2022-11-04 22:07:20 +0530

    gfg

--- GFG POTD/NOVEMBER/4_nov_base.cpp
+++ GFG POTD/NOVEMBER/4_nov_base.cpp
@@ -3,38 +3,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-// User function Template for C++
-class Solution {
-  public:
-    string baseEquiv(int n, int m){
+class Solution
+{
+public:
+    string baseEquiv(int n, int m)
+    {
         // code here
-        for(int i = 2;i<=32;i++){
+        for (int i = 2; i <= 32; i++)
+        {
             int digit = 0;
             int num = n;
-            while(num>0){
+            while (num > 0)
+            {
                 digit++;
-                num = num/i;
+                num = num / i;
             }
-            if(digit == m ) return "Yes";
-            
+            if (digit == m)
+                return "Yes";
         }
         return "No";
-        
-        
-        
     }
 };
 
 //{ Driver Code Starts.
-int main() {
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         int n, m;
         cin >> n >> m;
         Solution ob;
-        cout<<ob.baseEquiv(n,m)<<endl;
+        cout << ob.baseEquiv(n, m) << endl;
     }
     return 0;
 }
