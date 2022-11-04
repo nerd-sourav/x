1faef4cfd56d53f1a50629ee896ccd00ffb20b2a
nerd-sourav <souravprasad1869@gmail.com>
2022-11-04 22:07:09 +0530

    gfg

---
+++ GFG POTD/NOVEMBER/4_nov_base.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string baseEquiv(int n, int m){
+        // code here
+        for(int i = 2;i<=32;i++){
+            int digit = 0;
+            int num = n;
+            while(num>0){
+                digit++;
+                num = num/i;
+            }
+            if(digit == m ) return "Yes";
+            
+        }
+        return "No";
+        
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        Solution ob;
+        cout<<ob.baseEquiv(n,m)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
