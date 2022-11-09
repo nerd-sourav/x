3e4f510d88858c7f4568d98e225d8b742f825742
nerd-sourav <souravprasad1869@gmail.com>
2022-11-09 23:18:23 +0530

    gfg

--- GFG POTD/NOVEMBER/9_nov_minimumNumberOfPartiiton.java
+++ GFG POTD/NOVEMBER/9_nov_minimumNumberOfPartiiton.java
@@ -1,69 +1,55 @@
 
 import java.io.*;
 import java.util.*;
-class GfG
-{
-    public static void main(String args[])
-        {
-            Scanner sc = new Scanner(System.in);
-            int t = sc.nextInt();
-            while(t-->0)
-                {
-                    int n = sc.nextInt();
-                    int A[] = new int[n];
-                    for(int i = 0;i<n;i++)
-                        A[i] = sc.nextInt();
-                    Solution ob = new Solution();
-                    System.out.println(ob.minDifference(A,n));
-                }
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int A[] = new int[n];
+            for (int i = 0; i < n; i++)
+                A[i] = sc.nextInt();
+            Solution ob = new Solution();
+            System.out.println(ob.minDifference(A, n));
         }
-}    
-// } Driver Code Ends
+    }
+}
 
+class Solution {
 
+    public int minDifference(int arr[], int n) {
 
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        boolean dp[][] = new boolean[n + 1][sum + 1];
 
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                if (j == 0)
+                    dp[i][j] = true;
 
-class Solution
-{
+                else if (i == 0)
+                    dp[i][j] = false;
 
-	public int minDifference(int arr[], int n) 
-	{ 
-	    // Your code goes here
-	    // Your code goes here
-	     int sum = 0;
-	    for(int i=0; i<n; i++)
-	    {
-	        sum += arr[i];
-	    }
-	    boolean dp[][] = new boolean[n+1][sum+1];
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                    if (j >= arr[i - 1]) // sum>=ele
+                        dp[i][j] = dp[i][j] || dp[i - 1][j - arr[i - 1]];
+                }
 
-	    for(int i =0 ;i<=n ;i++)
-	    {
-	        for(int j =0; j<=sum; j++)
-	        {
-	            if(j==0) dp[i][j]=true;
-
-	            else if(i==0) dp[i][j] = false;
-
-	            else
-	            {
-	                dp[i][j] = dp[i-1][j];
-	                if(j>=arr[i-1]) // sum>=ele
-	                dp[i][j] = dp[i][j] || dp[i-1][j-arr[i-1]];
-	            }
-
-	        }
-	    }
-	    int ans = Integer.MAX_VALUE;
-	    for(int j=sum/2; j>=0 ;j--)
-	    {
-	        if(dp[n][j]==true)
-	        {
-	            ans = sum - 2 *j;
-	            break;
-	        }
-	    }
-	    return ans;
-	} 
+            }
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int j = sum / 2; j >= 0; j--) {
+            if (dp[n][j] == true) {
+                ans = sum - 2 * j;
+                break;
+            }
+        }
+        return ans;
+    }
 }
