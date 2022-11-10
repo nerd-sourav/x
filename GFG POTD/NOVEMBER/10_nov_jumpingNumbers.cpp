f5209e7bfc4d4bcc506d55d41f309c59cf7a330b
nerd-sourav <souravprasad1869@gmail.com>
2022-11-10 20:12:54 +0530

    gfg

--- GFG POTD/NOVEMBER/10_nov_jumpingNumbers.cpp
+++ GFG POTD/NOVEMBER/10_nov_jumpingNumbers.cpp
@@ -1,64 +1,70 @@
-//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
 // } Driver Code Ends
-class Solution {
-  public:
-    long long jumpingNums(long long x) {
+class Solution
+{
+public:
+    long long jumpingNums(long long x)
+    {
         // code here
-        if(x<=9) return x;
+        if (x <= 9)
+            return x;
         queue<int> q;
-        for(int i =0; i<=9;i++){
+        for (int i = 0; i <= 9; i++)
+        {
             q.push(i);
         }
         int ans = INT_MIN;
-        while(q.size()>0){
+        while (q.size() > 0)
+        {
             int digit = q.front();
             q.pop();
-            ans = max(ans,digit);
-            int rem = digit%10;
-            
-            int num1= digit*10+rem-1;
-            int num2 = digit*10+rem+1;
-            if(rem ==0){
-                if(num2<=x){
+            ans = max(ans, digit);
+            int rem = digit % 10;
+
+            int num1 = digit * 10 + rem - 1;
+            int num2 = digit * 10 + rem + 1;
+            if (rem == 0)
+            {
+                if (num2 <= x)
+                {
                     q.push(num2);
                 }
-                
             }
-            else if(rem==9){
-                if(num1<=x){
+            else if (rem == 9)
+            {
+                if (num1 <= x)
+                {
                     q.push(num1);
                 }
-                    
-                }
-            else{
-                if(num1<=x){
+            }
+            else
+            {
+                if (num1 <= x)
+                {
                     q.push(num1);
                 }
-                if(num2<=x){
+                if (num2 <= x)
+                {
                     q.push(num2);
                 }
-                
             }
-            
-            
         }
         return ans;
     }
 };
 
-
-
 //{ Driver Code Starts.
-int main() {
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         long long X;
-        
-        cin>>X;
+
+        cin >> X;
 
         Solution ob;
         cout << ob.jumpingNums(X) << endl;
