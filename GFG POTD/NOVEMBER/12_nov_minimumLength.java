54ac9f508e86adfc87ef606625db3a4030384dc0
nerd-sourav <souravprasad1869@gmail.com>
2022-11-12 23:21:49 +0530

    gfg

--- GFG POTD/NOVEMBER/12_nov_minimumLength.java
+++ GFG POTD/NOVEMBER/12_nov_minimumLength.java
@@ -1,55 +1,58 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s = br.readLine().trim();
+            int k = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int ans = ob.characterReplacement(s, k);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+// User function Template for Java
 
 class Solution {
-	public:
-		int characterReplacement(string s, int k){
-		    // Code here
-    		    int[]arr=new int[26];
-            int prev=0;
-            int ans=0;
-            for(int i=0;i<s.length();i++){
-                arr[s.charAt(i)-'A']++;
-                int curr=find(arr,k);
-                if(curr==-1){
-                    arr[s.charAt(prev++)-'A']--;
-                }else{
-                    ans=Math.max(ans,curr);
-                }
+    public int characterReplacement(String s, int k) {
+        // code here
+        int[] arr = new int[26];
+        int prev = 0;
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'A']++;
+            int curr = find(arr, k);
+            if (curr == -1) {
+                arr[s.charAt(prev++) - 'A']--;
+            } else {
+                ans = Math.max(ans, curr);
             }
-            return ans;
-    
         }
-        int find(int[]arr,int k){
-            int same=0;
-            int diff=0;
-            for(int i:arr){
-                if(i>same){
-                    diff+=same;
-                    same=i;
-                }else{
-                    diff+=i;
-                }
+        return ans;
+
+    }
+
+    int find(int[] arr, int k) {
+        int same = 0;
+        int diff = 0;
+        for (int i : arr) {
+            if (i > same) {
+                diff += same;
+                same = i;
+            } else {
+                diff += i;
             }
-            return diff<=k? same+diff : -1;
-    		}
-
-};
-
-//{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		string s;
-		cin >> s;
-		int k;
-		cin >> k;
-		Solution obj;
-		int ans = obj.characterReplacement(s, k);
-		cout << ans << "\n";
-	}
-	return 0;
+        }
+        return diff <= k ? same + diff : -1;
+    }
 }
-// } Driver Code Ends
