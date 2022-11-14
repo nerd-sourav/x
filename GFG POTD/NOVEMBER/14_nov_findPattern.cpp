7ae480c614df474b20729935f4643fde84988223
nerd-sourav <souravprasad1869@gmail.com>
2022-11-14 20:21:01 +0530

    gfg

---
+++ GFG POTD/NOVEMBER/14_nov_findPattern.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution
+{
+public:
+    int numberOfSubsequences(string s, string w)
+    {
+
+        vector<int> dp(s.size(), 0);
+        int i = 0, j = 0;
+        int ans = 0;
+        while (i < s.size())
+        {
+            if (s[i] == w[0] && dp[i] == 0)
+            {
+                s[i] = ' ';
+                j = 1;
+                int k = i + 1;
+                while (j < w.size() && k < s.size())
+                {
+                    if (s[k] == w[j])
+                    {
+                        s[k] = ' ';
+                        k++;
+                        j++;
+                    }
+                    else
+                        k++;
+                }
+                if (j == w.size())
+                    ans++;
+            }
+            i++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, W;
+        cin >> S >> W;
+        Solution ob;
+        cout << ob.numberOfSubsequences(S, W) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
