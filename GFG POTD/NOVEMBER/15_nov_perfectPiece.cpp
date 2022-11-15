eff63417b3202b22bfb97c75729cc3b9dd253bbf
nerd-sourav <souravprasad1869@gmail.com>
2022-11-15 20:44:15 +0530

    gfg

---
+++ GFG POTD/NOVEMBER/15_nov_perfectPiece.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int longestPerfectPiece(int arr[], int n)
+    {
+        // code here
+        int ans = 0;
+        map<int, int> mp;
+        for (int i = 0, j = 0; i < n; i++)
+        {
+            mp[arr[i]]++;
+            int mx = mp.rbegin()->first;
+            int mn = mp.begin()->first;
+            while (mx - mn > 1)
+            {
+                mp[arr[j]]--;
+                if (mp[arr[j]] == 0)
+                    mp.erase(arr[j]);
+                j++;
+                mx = mp.rbegin()->first;
+                mn = mp.begin()->first;
+            }
+            ans = max(ans, i - j + 1);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++)
+            cin >> arr[i];
+
+        Solution ob;
+        cout << ob.longestPerfectPiece(arr, N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
