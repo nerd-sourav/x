e8f3ff1ea14d9a8806d03338f6240fa10cbd353b
nerd-sourav <souravprasad1869@gmail.com>
2022-11-16 20:14:12 +0530

    gfg

---
+++ GFG POTD/NOVEMBER/16_nov_beautyString.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    int solve(vector<int> &dp)
+    {
+        int mx = INT_MIN;
+        int mn = INT_MAX;
+        for (int i = 0; i < dp.size(); i++)
+        {
+            if (dp[i] != 0)
+            {
+                mx = max(mx, dp[i]);
+                mn = min(mn, dp[i]);
+            }
+        }
+        return mx - mn;
+    }
+    int beautySum(string s)
+    {
+        // Your code goes here
+        int ans = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++)
+        {
+            vector<int> dp(26, 0);
+            for (int j = i; j < n; j++)
+            {
+                dp[s[j] - 'a'] += 1;
+                int diff = solve(dp);
+                ans = ans + diff;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout << obj.beautySum(s) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
