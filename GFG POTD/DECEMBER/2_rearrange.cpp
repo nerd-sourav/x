fe92e879017a937e9d4048ad47a0c4a9926649b4
nerd-sourav <souravprasad1869@gmail.com>
2022-12-02 22:22:16 +0530

    gfg

--- GFG POTD/DECEMBER/2_rearrange.cpp
+++ GFG POTD/DECEMBER/2_rearrange.cpp
@@ -1,4 +1,4 @@
-//{ Driver Code Starts
+
 // Initial Template for C++
 
 #include <bits/stdc++.h>
@@ -7,32 +7,34 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution {
-  public:
-    int isItPossible(string S, string T, int M, int N) {
+class Solution
+{
+public:
+    int isItPossible(string S, string T, int M, int N)
+    {
         // code here
-         if(M!=N) 
-           return 0;
-        
-        int i=0, j=0;
-        while(i<M || j<N)
+        if (M != N)
+            return 0;
+
+        int i = 0, j = 0;
+        while (i < M || j < N)
         {
-            while(S[i]=='#') 
-                  i++;
-            while(T[j]=='#')
-                   j++;
-            if(i>=M && j<N)
+            while (S[i] == '#')
+                i++;
+            while (T[j] == '#')
+                j++;
+            if (i >= M && j < N)
                 return 0;
-            if(i<M && j>=N)
-                  return 0;
-            if(S[i]!=T[j]) 
-                 return 0;
+            if (i < M && j >= N)
+                return 0;
+            if (S[i] != T[j])
+                return 0;
             else
             {
-                if(S[i]=='A' && i<j) 
-                      return 0;
-                else if(S[i]=='B' && i>j)
-                      return 0;
+                if (S[i] == 'A' && i < j)
+                    return 0;
+                else if (S[i] == 'B' && i > j)
+                    return 0;
             }
             i++;
             j++;
@@ -43,10 +45,12 @@ class Solution {
 
 //{ Driver Code Starts.
 
-int main() {
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         string S, T;
         cin >> S >> T;
         int M = S.length(), N = T.length();
