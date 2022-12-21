7dd626cf9180653bcfee52a89b03fa6e295c3612
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2022-12-21 17:32:44 +0530

    Create 21_dec.cpp

---
+++ GFG POTD/DECEMBER/21_dec.cpp
@@ -0,0 +1,14 @@
+Node* curr=*head;
+       while(curr!=NULL && curr->next!=NULL)
+       {
+           if(curr->next->data<0)
+           {
+               Node* temp=curr->next;
+               curr->next=curr->next->next;
+               temp->next=*head;
+               *head=temp;
+           }
+           else
+             curr=curr->next;
+       }
+       return *head;
