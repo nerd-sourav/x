fda825b8a53ae8939a1fb68359a78405a2b6cbce
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-01-08 20:59:17 +0530

    Create 8_janeasy.cpp

---
+++ GFG POTD/8_janeasy.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    long long countPairs(int n, int arr[], int k) {
+        // code here
+long long ans=0;
+        unordered_map<int,int> mm;
+        for(int i=0;i<n;i++)mm[arr[i]%k]++;
+        for(auto x:mm){
+            long long temp = (x.second*(x.second-1))/2;
+            ans+=temp;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,k;
+        cin>>n;
+        
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        
+        cin>>k;
+
+        Solution ob;
+        cout << ob.countPairs(n,arr,k) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
