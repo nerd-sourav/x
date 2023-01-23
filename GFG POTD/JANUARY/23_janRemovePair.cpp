fe9f66575a389dfb3fb9dfae7312f0bebb77ed9a
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-01-23 20:37:24 +0530

    Create 23_janRemovePair.cpp

---
+++ GFG POTD/JANUARY/23_janRemovePair.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string removePair(string s) {
+        // code here
+stack<char>st;
+
+        
+        for(int i=0; i<s.size(); i++)
+        {
+            if(!st.empty() and st.top()==s[i])
+            {
+                st.pop();
+            }
+            else 
+            {
+                st.push(s[i]);
+            }
+        }
+        
+        
+        string ans="";
+        
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        if(ans.size()==0)return "-1";
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s; 
+        cin>>s;
+        
+        Solution obj;
+        string res = obj.removePair(s);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
