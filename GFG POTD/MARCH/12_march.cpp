afba17cd5610a9f6a4869df6e99ace44a499a0b7
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-03-12 21:07:55 +0530

    Create 12_march.cpp

---
+++ GFG POTD/MARCH/12_march.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    vector<int> findMaxRow(vector<vector<int>> mat, int N) {
+        //code here
+int one=0,row=0,j=N-1;
+        for(int i=0;i<N;i++){
+            while(j>=0 and mat[i][j]){
+                j--;
+                one++;
+                row=i;
+            }
+        }
+        return {row,one};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        vector<vector<int>> arr(n, vector<int> (n));
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                cin >> arr[i][j];
+        Solution obj;
+        vector<int> ans = obj.findMaxRow(arr, n);
+        for(int val : ans) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends
