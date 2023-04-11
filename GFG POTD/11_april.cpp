6d8ee6f2e64239160a9bb37bcc5a8d66574becf0
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-04-11 20:08:16 +0530

    Create 11_april.cpp

---
+++ GFG POTD/11_april.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    int solve(int a, int b, int c) {
+        // code here
+if(a>(2*(b+c) +2)||b>(2*(a+c) +2)||c>(2*(b+a) +2)) return -1;
+        return a+b+c;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        Solution ob;
+        int ans = ob.solve(a, b, c);
+     
+
+        cout <<  ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
