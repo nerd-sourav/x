8fc5b42961d9c7c0ddbe55fba93b5f7a295df3a2
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-04-15 20:50:06 +0530

    Create 15_april.cpp

---
+++ GFG POTD/15_april.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int totalTime(int n, vector<int> &arr, vector<int> &time) {
+        // code here
+unordered_map<int,int> mm;
+        int ans = -1;
+        for(int i=0;i<n;i++){
+            ans++;
+            if(mm[arr[i]]){
+                ans+=time[arr[i]-1];
+                ans--;
+            }
+            mm[arr[i]]++;
+        }
+        return ans;
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> arr(n);
+        Array::input(arr,n);
+        
+        
+        vector<int> time(n);
+        Array::input(time,n);
+        
+        Solution obj;
+        int res = obj.totalTime(n, arr, time);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
