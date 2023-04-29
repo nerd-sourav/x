1389d44cc99d35205a87d1648ffe7285d48c4f80
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-04-29 21:03:50 +0530

    Create 29_april.cpp

---
+++ GFG POTD/APRIL/29_april.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    long long findNumber(long long n){
+        // Code here
+vector<int> v = {9, 1, 3, 5, 7};
+        
+        function<string(long long)> base5 = [&](long long x) -> string {
+        	if(x == 0)
+        		return "";
+    
+        	string ans = base5(x / 5 - ((x % 5 == 0) ? 1 : 0)) + to_string(v[x % 5]);
+        	return ans;
+        };
+        
+        return stol(base5(n));
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long N;
+        cin>>N;
+        Solution ob;
+        cout<<ob.findNumber(N)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
