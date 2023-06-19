9707d545694843d5487ea63cf54ec8aeaff56950
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-06-19 20:20:36 +0530

    Create 19_june.cpp

---
+++ GFG POTD/19_june.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+    
+
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to rearrange an array so that arr[i] becomes arr[arr[i]]
+    //with O(1) extra space.
+    void arrange(long long arr[], int n) {
+        // Your code here
+        for(int i = 0; i < n; i++){
+            long long need = arr[arr[i]];
+            for(int j = 0; j < 17; j++){
+                if((need >> j) & 1)
+                    arr[i] |= (1LL << (j + 17));
+            }
+        }
+        
+        for(int i = 0; i < n; i++)
+            arr[i] >>= 17;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin>>t;
+    while(t--){
+        
+        int n;
+        //size of array
+        cin>>n;
+        long long A[n];
+        
+        //adding elements to the array
+        for(int i=0;i<n;i++){
+            cin>>A[i];
+        }
+        Solution ob;
+        //calling arrange() function
+        ob.arrange(A, n);
+        
+        //printing the elements 
+        for(int i=0;i<n;i++){
+            cout << A[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
