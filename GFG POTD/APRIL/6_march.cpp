1ef3b5320e5bb34001684269c432112da974f490
Sourav Prasad <souravprasad1869@gmail.com>
2023-06-20 21:40:52 +0530

    no

--- GFG POTD/APRIL/6_march.cpp
+++ GFG POTD/APRIL/6_march.cpp
@@ -1,20 +1,19 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 
 class Array
 {
 public:
     template <class T>
-    static void input(vector<T> &A,int n)
+    static void input(vector<T> &A, int n)
     {
         for (int i = 0; i < n; i++)
         {
-            scanf("%d ",&A[i]);
+            scanf("%d ", &A[i]);
         }
     }
- 
+
     template <class T>
     static void print(vector<T> &A)
     {
@@ -26,50 +25,48 @@ public:
     }
 };
 
-
 // } Driver Code Ends
-class Solution {
-  public:
-    int equalSum(int N, vector<int> &A) {
+class Solution
+{
+public:
+    int equalSum(int N, vector<int> &A)
+    {
         // code here
-         int sum=0;
-		for(int i=0;i<N; i++)
-		sum+=A[i];
-		int lsum=0;
-		for(int i=0; i<N; i++)
-		{
-		    sum-=A[i];
-		    if(sum==lsum)
-		    return i+1;
-		    
-		    else 
-		    lsum+=A[i];
-		}
-		return -1;
+        int sum = 0;
+        for (int i = 0; i < N; i++)
+            sum += A[i];
+        int lsum = 0;
+        for (int i = 0; i < N; i++)
+        {
+            sum -= A[i];
+            if (sum == lsum)
+                return i + 1;
+
+            else
+                lsum += A[i];
+        }
+        return -1;
     }
 };
 
-
 //{ Driver Code Starts.
 
-int main(){
+int main()
+{
     int t;
-    scanf("%d ",&t);
-    while(t--){
-        
-        int N; 
-        scanf("%d",&N);
-        
-        
+    scanf("%d ", &t);
+    while (t--)
+    {
+
+        int N;
+        scanf("%d", &N);
+
         vector<int> A(N);
-        Array::input(A,N);
-        
+        Array::input(A, N);
+
         Solution obj;
         int res = obj.equalSum(N, A);
-        
-        cout<<res<<endl;
-        
+
+        cout << res << endl;
     }
 }
-
-// } Driver Code Ends
