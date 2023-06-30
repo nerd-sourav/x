7ed85dcb6efe3b7127209d7c6682288373be6f8a
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-06-30 20:00:14 +0530

    Create 30_june.cpp

---
+++ GFG POTD/JUNE/30_june.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	int isDivisible(string s){
+	    //complete the function here
+	    int cur = 0;
+	    
+	    for(auto i : s){
+	        cur = (cur * 2 + (i - '0')) % 3;
+	    }
+	    
+	    return cur == 0;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+    
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.isDivisible(s) << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
