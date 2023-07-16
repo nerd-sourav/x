9e47f40e2bff282b19451d1b5b8ea0373aca55f1
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-07-16 21:31:49 +0530

    Create 16_july.cpp

---
+++ GFG POTD/JULY/16_july.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//function Template for C++
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        // add code here.
+        int size=q.size();
+        vector<int> arr;
+        while(size--){
+            int x=q.front();
+            q.pop();
+            arr.push_back(x);
+        }
+        size=arr.size();
+        for(int i=size-1;i>=0;i--){
+            q.push(arr[i]);
+        }
+        return q;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    Solution ob;
+    queue<int> a=ob.rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+// } Driver Code Ends
