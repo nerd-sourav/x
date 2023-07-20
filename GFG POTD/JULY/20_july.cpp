033f76f66c658da33a2134317a25fdb67f9f7cab
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-07-20 19:38:55 +0530

    Create 20_july.cpp

---
+++ GFG POTD/JULY/20_july.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find the first non-repeating character in a string.
+    char nonrepeatingCharacter(string S)
+    {
+       //Your code here
+        unordered_map<char,int> mm;
+       for(auto x:S)mm[x]++;
+       for(auto x:S)if(mm[x]==1)return x;
+       return '$';
+       
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int T;
+	cin >> T;
+	
+	while(T--)
+	{
+	
+	    string S;
+	    cin >> S;
+	    Solution obj;
+        char ans = obj.nonrepeatingCharacter(S);
+        
+        if(ans != '$')
+	    cout << ans;
+        else cout << "-1";
+            
+        cout << endl;
+	    
+	}
+	
+	return 0;
+}
+
+// } Driver Code Ends
