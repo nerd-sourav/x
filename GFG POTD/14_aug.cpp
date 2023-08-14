7ddb3cc1096eaf7e2a4e3f74aa3a7568add48067
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-08-14 19:04:30 +0530

    Create 14_aug.cpp

---
+++ GFG POTD/14_aug.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+        // Code here
+        int value = 0;
+        for(auto i : nums)
+            value ^= i;
+        
+        int rightmost = (value & (value - 1)) ^ value;
+        
+        vector<int> ans(2, 0);
+        
+        for(auto i : nums){
+            if(i & rightmost)
+                ans[0] ^= i;
+            else
+                ans[1] ^= i;
+        }
+        
+        if(ans[0] > ans[1])
+            swap(ans[0], ans[1]);
+            
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(2 * n + 2);
+    	for(int i = 0; i < 2 * n + 2; i++)
+    		cin >> v[i];
+    	Solution ob;
+    	vector<int > ans = ob.singleNumber(v);
+    	for(auto i: ans)
+    		cout << i << " ";
+    	cout << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
