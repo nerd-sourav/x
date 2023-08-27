069357b4f85ba1d9369b1b967e034078824d97cd
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-08-27 22:03:15 +0530

    Create 27_aug.cpp

---
+++ GFG POTD/27_aug.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+        for(int i = 0; i < str.size() / 2; i++){
+            swap(str[i], str[str.size() - i - 1]);
+        }
+        
+        return str;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
