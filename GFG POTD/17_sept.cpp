098f9795a27ccf1b8e40ca3836e406ea4f9d0894
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-09-17 21:52:56 +0530

    Create 17_sept.cpp

---
+++ GFG POTD/17_sept.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+
+class Solution
+{
+    public:
+    //Function to return list containing first n fibonacci numbers.
+    vector<long long> printFibb(int n) 
+    {
+        //code here
+        vector<long long>ans;
+        ans.push_back(1);
+        long long f=0;
+        long long s=1;
+        long long num;
+        while(n>1){
+            num=f+s;
+            ans.push_back(num);
+            f=s;
+            s=num;
+            n--;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking number of elements
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling function printFibb()
+        vector<long long> ans = obj.printFibb(n);
+        
+        //printing the elements of vector
+        for(long long i:ans)cout<<i<<' ';
+        cout<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
