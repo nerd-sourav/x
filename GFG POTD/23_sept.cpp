8bfbeccc825aabb0deaeb027c6613c219774765e
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-09-23 21:53:40 +0530

    Create 23_sept.cpp

---
+++ GFG POTD/23_sept.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+    
+        // Your code here
+        long long sumArray=0;
+        
+        for(int i=0;i<n;i++) sumArray+=a[i];
+        
+        long long ss=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(ss==sumArray-ss-a[i]) return i+1;
+            ss+=a[i];
+        }
+        
+            return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
