b016b34ee0aa456b6d80c1e42a58ab0f27e4c3eb
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-09-27 21:58:47 +0530

    Create 27_sept.cpp

---
+++ GFG POTD/27_sept.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function teamplate for C++
+
+class Solution{
+  public:
+    vector<int> printClosest(int arr[], int brr[], int n, int m, int x) {
+        //code here
+        vector<int> ans(2);
+        int mn=INT_MAX;
+        int i=0;
+        int j=m-1;
+        int a=-1,b=-1;
+        while(i<n && j>=0){
+            int sum=arr[i]+brr[j];
+            int diff=abs(sum-x);
+            if(diff< mn){
+                mn=diff;
+                ans[0]=arr[i];
+                ans[1]=brr[j];
+            }
+            if(sum>x) j--;
+            else i++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	int m,n,x;
+	while(t--)
+    {
+        cin>>n>>m;
+        int a[n],b[m];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+            
+        for(int i=0;i<m;i++)
+            cin>>b[i];
+            
+        cin>>x;
+        
+        vector<int> ans;
+        Solution ob;
+        ans = ob.printClosest(a, b, n, m, x);
+        cout << abs(ans[0] + ans[1] - x) << endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
