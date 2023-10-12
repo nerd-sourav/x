fbf1149ca98300fcc82caf8902e702e47a1faab3
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-10-12 23:19:55 +0530

    Create 12_oct.cpp

---
+++ GFG POTD/12_oct.cpp
@@ -0,0 +1,144 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	char data;
+	struct Node *left;
+	struct Node *right;
+
+	Node(char x) {
+		data = x;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+struct Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node *root = new Node(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current Node
+			currNode->left = new Node(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = new Node(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+
+// } Driver Code Ends
+/*The structure of the Binary Tree Node  is
+struct Node
+{
+  char data;
+  struct Node* left;
+  struct Node* right;
+};*/
+
+class Solution {
+  public:
+    /*This function returns true if the tree contains 
+    a duplicate subtree of size 2 or more else returns false*/
+    int duhpSub(Node *root) {
+         // code here
+    }
+    
+    int dupSub(Node* root) {
+        unordered_map<string, int> hm;
+        solve(root->left, hm);
+        solve(root->right, hm);
+
+        for (const auto& e : hm)
+            if (e.second > 1)
+                return 1;
+        return 0;
+    }
+
+    string solve(Node* node, unordered_map<string, int>& hm) {
+        string sb;
+        if (node == nullptr)
+            return "n";
+        if (node->right == nullptr && node->left == nullptr)
+            return to_string(node->data) + ",";
+
+        sb.append(solve(node->left, hm));
+        sb.append(solve(node->right, hm));
+        sb.append(to_string(node->data) + ",");
+        
+        hm[sb] = hm[sb] + 1;
+        return sb;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	
+	int t;
+	cin >> t;
+	//cout << t << "\n";
+	while (t--)
+	{
+		string treeString;
+		getline(cin >> ws, treeString);
+		struct Node* root = buildTree(treeString);
+		Solution ob;
+		cout << ob.dupSub(root) << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
