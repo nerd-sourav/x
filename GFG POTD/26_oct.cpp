605c795a9f5a6753a8b2d3c36d5a66f97c9d941e
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-10-26 23:16:05 +0530

    Create 26_oct.cpp

---
+++ GFG POTD/26_oct.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minOperation(n));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    public int minOperation(int n)
+    {
+        
+        int cnt=0;
+        while(n!=0){
+            if(n%2==0){
+                n/=2;
+            }
+            else{
+                n-=1;
+            }
+            cnt++;
+        }
+        return cnt;
+    }
+}
