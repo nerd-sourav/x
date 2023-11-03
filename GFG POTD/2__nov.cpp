005575c9c8c603941196e8267be23ab8d747b3e5
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-11-03 23:13:46 +0530

    Create 2__nov.cpp

---
+++ GFG POTD/2__nov.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// Function to check if the
+	// Pythagorean triplet exists or not
+	bool checkTriplet(int arr[], int n) {
+	    // code here
+	    unordered_map<int,int> mp;
+	    for(int i=0;i<n;i++){
+            mp[pow(arr[i],2)]++;
+	    }
+	    for(int i=0;i<n;i++){
+	        for(int j=i+1;j<n;j++){
+	            int sum=pow(arr[i],2)+pow(arr[j],2);
+	            if(mp[sum]>0) return true;
+	        }
+	    }
+	    return false;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.checkTriplet(arr, n);
+        if (ans) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+    return 0;
+}
+
+// } Driver Code Ends
