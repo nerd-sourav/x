0611e884afa032d6f44ebe33e10e3235f6b70029
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-11-09 22:14:05 +0530

    Create 9_nov.cpp

---
+++ GFG POTD/9_nov.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    /*Function to count zeros in each column
+    * N : Number of rows and columns in array
+    M is the matrix that is globally declared
+    */
+    int columnWithMaxZeros(vector<vector<int>>arr,int N){
+        
+        // Your code here
+         int res = -1;
+        int b = 0;
+
+        for (int i = 0; i < N; i++) {
+            int zero = 0;
+            for (int j = 0; j < N; j++) {
+                if (arr[j][i] == 0) zero++;
+            }
+            if (zero > b) res = i;
+            b = max(b, zero);
+        }
+        return res;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	
+	int testcase;
+	
+	cin >> testcase;
+	
+	while(testcase--){
+	    int N;
+	    cin >> N;
+	    vector<vector<int>>arr(N,vector<int>(N));
+	    // Taking elements input into the matrix
+	    for(int i = 0;i<N;i++){
+	        for(int j = 0;j<N;j++){
+	            int x;
+	            cin >> x; 
+	            arr[i][j]=x;
+	        }
+	    }
+	    Solution ob;
+	    cout << ob.columnWithMaxZeros(arr,N) << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
