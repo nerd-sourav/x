a59d9ec16b043e2cb8f8001f167b945c0f2c5d2c
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-11-21 23:02:59 +0530

    Create 21_nov.cpp

---
+++ GFG POTD/21_nov.cpp
@@ -0,0 +1,7 @@
+if(r1 == NULL && r2 == NULL)return true;
+
+        if(r1 == NULL || r2 == NULL)return false;
+
+        if(r1->data != r2->data)  return false;
+
+        return (isIdentical(r1->left,r2->left)&&isIdentical(r1->right,r2->right));      
