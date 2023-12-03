efd059bbac1f83585b420919ef1adb8b5ec0e22b
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-12-03 23:30:55 +0530

    Create 3_dec.cpp

---
+++ GFG POTD/3_dec.cpp
@@ -0,0 +1,153 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*Structure of the Node of the BST is as
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+static int countPairs(Node* root1, Node* root2, int x) {
+        std::unordered_set<int> set;
+        int arr[] = {0};
+        inorder(root1, set, x, arr, true);
+        inorder(root2, set, x, arr, false);
+
+        return arr[0];
+    }
+  private:
+    static void inorder(Node* node, std::unordered_set<int>& set, int x, int arr[], bool flag) {
+        if (node == nullptr) return;
+
+        if (flag) {
+            inorder(node->left, set, x, arr, true);
+            if (node->data < x) set.insert(node->data);
+            inorder(node->right, set, x, arr, true);
+        } else {
+            inorder(node->left, set, x, arr, false);
+            if (set.count(x - node->data)) arr[0]++;
+            inorder(node->right, set, x, arr, false);
+        }
+    
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        string tree1, tree2;
+        getline(cin, tree1);
+        getline(cin, tree2);
+        Node* root1 = buildTree(tree1);
+        Node* root2 = buildTree(tree2);
+        int x;
+        cin >> x;
+        cin.ignore();
+        Solution ob;
+        cout << ob.countPairs(root1, root2, x) << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
