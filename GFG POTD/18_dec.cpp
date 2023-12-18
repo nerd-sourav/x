dd689870375c13bdda77c6dcaabe2e2c9a1f323d
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-12-18 23:22:11 +0530

    Create 18_dec.cpp

---
+++ GFG POTD/18_dec.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int gameOfXor(int N , int A[]) {
+        int x = 0;
+
+        for(int i=0; i<N; i++){
+            int y = (i+1)*(N-i);
+            if(y%2==1) x=x^A[i];
+        }
+        return x;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int A[N];
+        for(int i=0 ; i<N ; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.gameOfXor(N,A) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
