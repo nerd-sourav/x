a7f7cd8fe8c3baf0befb3d0b705dbe4adcaa54bd
Sourav Prasad <72149259+nerd-sourav@users.noreply.github.com>
2023-12-24 22:11:55 +0530

    Create 24_dec.cpp

---
+++ GFG POTD/24_dec.cpp
@@ -0,0 +1,21 @@
+ long long arr[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = (long long)price[i] * 100000 + (i + 1);
+        }
+        sort(arr, arr + n);
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int day = (int)(arr[i]) % 100000;
+            int pr = (int)(arr[i]) / 100000;
+
+            if ((long long)pr * day <= k) {
+                count += day;
+                k = k - (long long)pr * day;
+            }
+            else {
+                count += (k / pr);
+                k = k - (long long)pr * (k / pr);
+            }
+        }
+
+        return count;
